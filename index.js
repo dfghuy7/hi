9310b0330c54d92b240a2b29af6139fa21d545ff
dfghuy7 <gs1alper15a@hotmail.com>
2024-05-08 08:50:32 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const gcd = (a, b) => {
-  if (b === 0) {
-    return a;
-  }
-  return gcd(b, a % b);
-};
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size),
+  );
